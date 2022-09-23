06559971873c4d50585066b2f185e9ec35175376
DarkLocuts <darklocuts23@gmail.com>
2022-09-23 22:42:53 +0700

    fix: button component

--- components/base_component/ButtonComponent.jsx
+++ components/base_component/ButtonComponent.jsx
@@ -30,15 +30,17 @@ function ButtonComponent({
       onClick={(e) => onClick && onClick(e)}
       className={`
         relative overflow-hidden 
-        ${square ? size == "sm" ? "py-3 px-3" : size == "lg" ? "py-5 px-5" : "py-4 px-5"
-          : boxIcon ? size == "sm" ? "py-2 pl-2 pr-6" : size == "lg" ? "py-3 pl-3 pr-12" : "py-3 pl-3 pr-10"
+        ${square ? size == "sm" ? "w-[35px] aspect-square flex items-center justify-center" : size == "lg" ? "w-[55px] aspect-square flex items-center justify-center" : "w-[45px] aspect-square flex items-center justify-center"
+          : boxIcon ? size == "sm" ? "py-2 pl-2 pr-6" : size == "lg" ? "py-3 pl-3 pr-12" : "py-2.5 pl-2.5 pr-10"
             : size == "sm" ? "py-2 px-8" : size == "lg" ? "py-4 px-12" : "py-3 px-10"
         } 
         ${rounded ? "rounded-full" : "rounded-lg"} 
         ${block ? "block w-full" : "w-auto"} 
         ${border ? "border-[2px] border__" + border : ""} 
-        ${disabled ? "btn__disabled" : `bg__${bg}`
-      } ${className}`}>
+        ${disabled ? "btn__disabled" : `bg__${bg}`}
+        ${loading ? "opacity-80" : ""}
+        ${className}
+      `}>
 
       <div
         className={`
@@ -49,48 +51,52 @@ function ButtonComponent({
           ${size == "sm" ? "text-base" : size == "lg" ? "text-2xl" : "text-xl"}
         `}
       >
-        {!loading && (
-          <>
-            {icon && !boxIcon && (
-              <FontAwesomeIcon
-                icon={icon}
-                className={`
-                  inline 
-                  ${size == "sm" ? "text-base" : size == "lg" ? "text-2xl" : "text-xl"}
-                `}
-              />
-            )}
 
-            {icon && boxIcon && (
-              <div
-                className={`
-                  flex items-center justify-center bg-white bg-opacity-20
-                  ${size == "sm" ? "w-6 h-6" : size == "lg" ? "w-12 h-12" : "w-9 h-9"} 
-                  ${rounded ? "rounded-full" : "rounded-md"}
-                `}
-              >
+        <>
+          {!loading && (
+            <>
+              {icon && !boxIcon && (
                 <FontAwesomeIcon
                   icon={icon}
                   className={`
-                    inline
-                    ${size == "sm" ? "text-sm" : size == "lg" ? "text-xl" : "text-base"}
+                    inline 
+                    ${size == "sm" ? "text-lg" : size == "lg" ? "text-2xl" : "text-xl"}
                   `}
                 />
-              </div>
-            )}
-            {label}
-          </>
-        )}
+              )}
 
-        {loading && (
-          <FontAwesomeIcon
-            icon={faSpinner}
-            className={`
-              btn__loading 
-              ${size == "sm" ? "w-5 h-5" : size == "lg" ? "w-9 h-9" : "w-7 h-7"} 
-            `}
-          />
-        )}
+              {icon && boxIcon && (
+                <div
+                  className={`
+                    flex items-center justify-center bg-white bg-opacity-20
+                    ${size == "sm" ? "w-6 h-6" : size == "lg" ? "w-10 h-10" : "w-8 aspect-square"} 
+                    ${rounded ? "rounded-full" : "rounded-md"}
+                  `}
+                >
+                  <FontAwesomeIcon
+                    icon={icon}
+                    className={`
+                      inline
+                      ${size == "sm" ? "text-sm" : size == "lg" ? "text-xl" : "text-base"}
+                    `}
+                  />
+                </div>
+              )}
+            </>
+          )}
+
+          {loading && (
+            <>
+              <div className={`
+                  btn__loading variant_${color}
+                  ${size == "sm" ? "w-3 h-3" : size == "lg" ? "w-5 h-5" : "w-4 h-4"}
+              `}></div>
+            </>
+
+          )}
+          {label}
+        </>
+
       </div>
     </button>
   );
--- pages/button-component.jsx
+++ pages/button-component.jsx
@@ -5,170 +5,214 @@ import { ButtonComponent, InputDefaultComponent } from '../components/base_compo
 const ButtonComponentPage = () => {
     return (
         <>
-            <div className='container mx-auto items-center flex flex-wrap my-5 gap-5'>
-                <ButtonComponent
-                    label={"Button"}
-                    bg="primary"
-                    size={"sm"}
-                />
-                <ButtonComponent
-                    label={"Button"}
-                    bg="primary"
-                />
-                <ButtonComponent
-                    label={"Button"}
-                    bg="primary"
-                    size={"lg"}
-                />
+            <div className='container mx-auto items-center my-5'>
+                <div className='flex flex-col gap-10'>
+                    <div className='flex items-center gap-5 flex-wrap'>
+                        <ButtonComponent
+                            label={"Button"}
+                            bg="primary"
+                            size={"sm"}
+                        />
 
-                <ButtonComponent
-                    label={"Button"}
-                    bg="primary"
-                    size={"sm"}
-                    icon={faPaperPlane}
-                />
-                <ButtonComponent
-                    label={"Button"}
-                    bg="primary"
-                    icon={faPaperPlane}
-                />
-                <ButtonComponent
-                    label={"Button"}
-                    bg="primary"
-                    size={"lg"}
-                    icon={faPaperPlane}
-                />
+                        <ButtonComponent
+                            label={"Button"}
+                            bg="primary"
+                            size={"sm"}
+                            icon={faPaperPlane}
+                        />
 
-                <ButtonComponent
-                    label={"Button"}
-                    bg="primary"
-                    size={"sm"}
-                    icon={faPaperPlane}
-                    boxIcon
-                />
-                <ButtonComponent
-                    label={"Button"}
-                    bg="primary"
-                    icon={faPaperPlane}
-                    boxIcon
-                />
-                <ButtonComponent
-                    label={"Button"}
-                    bg="primary"
-                    size={"lg"}
-                    icon={faPaperPlane}
-                    boxIcon
-                />
+                        <ButtonComponent
+                            label={"Button"}
+                            bg="primary"
+                            loading
+                            size={"sm"}
+                        />
 
-                <ButtonComponent
-                    label={"Button"}
-                    bg="primary"
-                    icon={faPaperPlane}
-                    boxIcon
-                    size={"sm"}
-                    rounded
-                />
-                <ButtonComponent
-                    label={"Button"}
-                    bg="primary"
-                    icon={faPaperPlane}
-                    boxIcon
-                    rounded
-                />
-                <ButtonComponent
-                    label={"Button"}
-                    bg="primary"
-                    icon={faPaperPlane}
-                    boxIcon
-                    rounded
-                    size={"lg"}
-                />
+                        <ButtonComponent
+                            label={"Button"}
+                            bg="primary"
+                            icon={faPaperPlane}
+                            size={"sm"}
+                            rounded
+                        />
 
-                <ButtonComponent
-                    square
-                    bg="primary"
-                    size={"sm"}
-                    icon={faPaperPlane}
-                />
-                <ButtonComponent
-                    square
-                    bg="primary"
-                    icon={faPaperPlane}
-                />
-                <ButtonComponent
-                    square
-                    bg="primary"
-                    size={"lg"}
-                    icon={faPaperPlane}
-                />
+                        <ButtonComponent
+                            label={"Button"}
+                            bg="primary"
+                            loading
+                            size={"sm"}
+                            rounded
+                        />
 
-                <ButtonComponent
-                    label={"Button"}
-                    bg="primary"
-                    loading
-                    size={"sm"}
-                />
-                <ButtonComponent
-                    label={"Button"}
-                    bg="primary"
-                    loading
-                />
-                <ButtonComponent
-                    label={"Button"}
-                    bg="primary"
-                    loading
-                    size={"lg"}
-                />
+                        <ButtonComponent
+                            square
+                            bg="primary"
+                            size={"sm"}
+                            icon={faPaperPlane}
+                        />
 
-                <ButtonComponent
-                    label={"Button"}
-                    bg="primary"
-                    loading
-                    size={"sm"}
-                    square
-                />
-                <ButtonComponent
-                    label={"Button"}
-                    bg="primary"
-                    loading
-                    square
-                />
-                <ButtonComponent
-                    label={"Button"}
-                    bg="primary"
-                    loading
-                    size={"lg"}
-                    square
-                />
+                        <ButtonComponent
+                            bg="primary"
+                            loading
+                            size={"sm"}
+                            square
+                        />
 
-                <ButtonComponent
-                    label={"Button"}
-                    bg="light__primary"
-                    color={"primary"}
-                    rounded
-                />
+                        <ButtonComponent
+                            label={"Button"}
+                            bg="primary"
+                            icon={faPaperPlane}
+                            boxIcon
+                            size={"sm"}
+                        />
+                    </div>
 
-                <ButtonComponent
-                    label={"Button"}
-                    border="primary"
-                    color={"primary"}
-                    rounded
-                />
+                    <div className='flex flex-wrap gap-5 items-start'>
+                        <ButtonComponent
+                            label={"Button"}
+                            bg="primary"
+                        />
 
-                <ButtonComponent
-                    label={"Button"}
-                    border="primary"
-                    bg="light__primary"
-                    color={"primary"}
-                    rounded
-                />
+                        <ButtonComponent
+                            label={"Button"}
+                            bg="primary"
+                            icon={faPaperPlane}
+                        />
 
-                <ButtonComponent
-                    label={"Button"}
-                    bg="primary"
-                    disabled
-                    rounded
-                />
+                        <ButtonComponent
+                            label={"Button"}
+                            bg="primary"
+                            loading
+                        />
+
+                        <ButtonComponent
+                            label={"Button"}
+                            bg="primary"
+                            icon={faPaperPlane}
+                            rounded
+                        />
+
+                        <ButtonComponent
+                            label={"Button"}
+                            bg="primary"
+                            loading
+                            rounded
+                        />
+
+                        <ButtonComponent
+                            square
+                            bg="primary"
+                            icon={faPaperPlane}
+                        />
+
+                        <ButtonComponent
+                            bg="primary"
+                            loading
+                            square
+                        />
+
+                        <ButtonComponent
+                            label={"Button"}
+                            bg="primary"
+                            icon={faPaperPlane}
+                            boxIcon
+                        />
+                    </div>
+
+                    <div className='flex flex-wrap gap-5 items-start'>
+                        <ButtonComponent
+                            label={"Button"}
+                            bg="primary"
+                            size={"lg"}
+                        />
+
+                        <ButtonComponent
+                            label={"Button"}
+                            bg="primary"
+                            size={"lg"}
+                            icon={faPaperPlane}
+                        />
+
+                        <ButtonComponent
+                            label={"Button"}
+                            bg="primary"
+                            loading
+                            size={"lg"}
+                        />
+
+                        <ButtonComponent
+                            label={"Button"}
+                            bg="primary"
+                            icon={faPaperPlane}
+                            size={"lg"}
+                            rounded
+                        />
+
+                        <ButtonComponent
+                            label={"Button"}
+                            bg="primary"
+                            loading
+                            size={"lg"}
+                            rounded
+                        />
+
+                        <ButtonComponent
+                            square
+                            bg="primary"
+                            size={"lg"}
+                            icon={faPaperPlane}
+                        />
+
+                        <ButtonComponent
+                            bg="primary"
+                            loading
+                            size={"lg"}
+                            square
+                        />
+
+                        <ButtonComponent
+                            label={"Button"}
+                            bg="primary"
+                            icon={faPaperPlane}
+                            boxIcon
+                            size={"lg"}
+                        />
+                    </div>
+
+                    <div className='flex flex-wrap gap-5 items-start'>
+                        <ButtonComponent
+                            label={"Button"}
+                            bg="light__primary"
+                            color={"primary"}
+                            rounded
+                        />
+
+                        <ButtonComponent
+                            label={"Button"}
+                            border="primary"
+                            color={"primary"}
+                            rounded
+                        />
+
+                        <ButtonComponent
+                            label={"Button"}
+                            border="primary"
+                            bg="light__primary"
+                            color={"primary"}
+                            rounded
+                        />
+
+                        <ButtonComponent
+                            label={"Button"}
+                            bg="primary"
+                            disabled
+                            rounded
+                        />
+                    </div>
+                </div>
+
+
+
             </div>
         </>
     );
